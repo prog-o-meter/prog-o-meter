e7c7804a4c95b24292f6a14030bde984a139bd36
py-den <densolo777@gmail.com>
2018-06-03 10:27:27 +0300

    changed layout and added doc_strings

--- congratulations/Congratulations.py
+++ congratulations/Congratulations.py
@@ -31,29 +31,41 @@ class Congratulations(object):
         # Text label
         lbl = Tk.Label(self.root, text="Congratulations!\nYou've reached the end of your challenge! You've earned the right to boast a little, so why not tell everyone about your accomplishment?",
         anchor=Tk.CENTER, justify=Tk.CENTER, wraplength=CANVAS_WIDTH-20, padx=10)
-        lbl.place(relx=0.0, rely=0.33)
+        lbl.place(relx=0.0, rely=0.15)
 
         share_lbl = Tk.Label(self.root, text="Share on:")
-        share_lbl.pack(side=Tk.LEFT)
+        share_lbl.place(relx=0.33, rely=0.55)
         # Social media button
         twitter_btn = Tk.Button(self.root, text="Twitter", command=self.open_twitter_browser)
         vk_btn = Tk.Button(self.root, text="Vkontakte", command=self.open_vk_browser)
         facebook_btn = Tk.Button(self.root, text="Facebook", command=self.open_facebook_browser)
 
-        twitter_btn.pack(side=Tk.RIGHT)
-        facebook_btn.pack(side=Tk.RIGHT)
-        vk_btn.pack(side=Tk.RIGHT)
+        #configuring and placing buttons
+        twitter_btn.config(width=11)
+        facebook_btn.config(width=11)
+        vk_btn.config(width=11)
+        twitter_btn.place(relx=0.25, rely=0.63)
+        facebook_btn.place(relx=0.25, rely=0.74)
+        vk_btn.place(relx=0.25, rely=0.85)
 
         # Tkinter instantiation
         self.root.mainloop()
     
     def open_facebook_browser(self):
+        """Opens a new browser tab to the Facebook page for creating a post.
+
+        Pre-populates the post with a stock message.
+        """
         msg = quote("I just completed my " + str(self.challenge_duration) + "-days challenge! "+ self.challenge_hashtag)
         print(self.challenge_hashtag)
         print(msg)
         webbrowser.open_new_tab("https://www.facebook.com/sharer/sharer.php?u=https://github.com/prog-o-meter/prog-o-meter&quote=" + msg)
 
     def open_vk_browser(self):
+        """Opens a new browser tab to the Vkontakte page for creating a post.
+
+        Pre-populates the post with a stock message.
+        """
         msg = quote("I just completed my " + str(self.challenge_duration) + "-days challenge! "+ self.challenge_hashtag)
         print(self.challenge_hashtag)
         print(msg)
